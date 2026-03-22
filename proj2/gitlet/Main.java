7e647966ceca924cff12c9ce9df5067d34a72479
yunqi10086 <zzx2242170669@163.com>
2026-03-22 20:02:48 +0800

    a little bug2

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -14,35 +14,38 @@ public class Main {
      */
     public static void main(String[] args) throws IOException {
         // TODO: what if args is empty?
-        if(args.length == 0 || args == null){
-            System.out.println("Please enter a command.");
-            exit(0);
-        }
-        String firstArg = args[0];
-        switch(firstArg) {
-            case "init":
-                // TODO: handle the `init` command
-                Repository.init();
-                break;
-            case "add":
-                // TODO: handle the `add [filename]` command
-                Repository.add(args[1]);
-                break;
-            // TODO: FILL THE REST IN
-            case "commit":
-                if(args.length == 1){
-                    System.out.println("Please enter a commit message");
-                    exit(0);
-                }
-                else{
-                    Repository.commit(args[1]);
-                }
-                break;
-            case "rm":
-                Repository.rm(args[1]);
-            default:
-                System.out.println("No command with that name exists.");
+        try {
+            if (args.length == 0 || args == null) {
+                System.out.println("Please enter a command.");
                 exit(0);
+            }
+            String firstArg = args[0];
+            switch (firstArg) {
+                case "init":
+                    // TODO: handle the `init` command
+                    Repository.init();
+                    break;
+                case "add":
+                    // TODO: handle the `add [filename]` command
+                    Repository.add(args[1]);
+                    break;
+                // TODO: FILL THE REST IN
+                case "commit":
+                    if (args.length == 1) {
+                        System.out.println("Please enter a commit message");
+                        exit(0);
+                    } else {
+                        Repository.commit(args[1]);
+                    }
+                    break;
+                case "rm":
+                    Repository.rm(args[1]);
+                default:
+                    System.out.println("No command with that name exists.");
+                    exit(0);
+            }
+        } catch (Exception e){
+            System.exit(0);
         }
     }
 }
